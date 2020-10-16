7de3ff0f7055926650b316f029e88c1f205135a3
Kangwon Lee (Education) <kangwon.lee@kpu.ac.kr>
2020-10-16 07:32:13 +0000

    added print lines

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 def factorial(n=10):
+
+  print(f"factorial({n})")
+
   if n < 2:
+    print(f"n = {n} < 2")
     result = 1
   else:
+    print(f"n = {n} >= 2")
     result = factorial(n-1) * n
+  print("result =", result)
   return result
 
 n = 5
